f77b85c6e1671e4ede5202bc7d46ea90f81a88e4
aoarashi1988 <30466105+aoarashi1988@users.noreply.github.com>
2019-06-19 21:26:46 +0800

    整理props传递

--- src/QRScanner.js
+++ src/QRScanner.js
@@ -1,6 +1,6 @@
-import React, {PureComponent} from 'react';
-import { RNCamera } from 'react-native-camera';
-import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react'
+import RNCamera from 'react-native-camera'
+import PropTypes from 'prop-types'
 
 import {
   StyleSheet,
@@ -11,7 +11,7 @@ import {
   Platform,
   PixelRatio,
   StatusBar
-} from 'react-native';
+} from 'react-native'
 
 import QRScannerView from './QRScannerView'
 const pixelRatio = PixelRatio.get()
@@ -21,109 +21,111 @@ const pixelRatio = PixelRatio.get()
  */
 export default class QRScanner extends PureComponent {
   constructor(props) {
-    super(props);
+    super(props)
     //通过这句代码屏蔽 YellowBox
-    console.disableYellowBox = true;
+    console.disableYellowBox = true
     this.state = {
       scanning: false,
       barCodeSize: {}
     }
   }
 
-  static defaultProps =  {
-    onRead: ()=>{},
-    renderTopView: () =>{},
-    renderBottomView: ()=><View style={{flex:1,backgroundColor:'#0000004D'}}/>,
+  static defaultProps = {
+    onRead: () => {},
+    renderTopView: () => {},
+    renderBottomView: () => (
+      <View style={{ flex: 1, backgroundColor: '#0000004D' }} />
+    ),
     rectHeight: 200,
     rectWidth: 200,
-    flashMode: false,   // 手电筒模式
-    finderX: 0,         // 取景器X轴偏移量
-    finderY: 0,         // 取景器Y轴偏移量
-    zoom: 0.2,          // 缩放范围 0 - 1
+    flashMode: false, // 手电筒模式
+    finderX: 0, // 取景器X轴偏移量
+    finderY: 0, // 取景器Y轴偏移量
+    zoom: 0.2, // 缩放范围 0 - 1
     translucent: false,
     isRepeatScan: false
   }
-  
+
   render() {
+    const {
+      flashMode,
+      renderTopView,
+      renderBottomView,
+      topViewStyle,
+      bottomViewStyle,
+      onRead,
+      ...ScanViewProps
+    } = this.props
     return (
-      <View style={{
-        flex: 1
-      }}>
-        <RNCamera
-         style={{
-          flex: 1
+      <View
+        style={{
+          flex: 1,
         }}
+        >
+        <RNCamera
+          style={{
+            flex: 1,
+          }}
           onBarCodeRead={this._handleBarCodeRead}
-          barCodeTypes={[RNCamera.Constants.BarCodeType.qr]}
-          flashMode={!this.props.flashMode ? RNCamera.Constants.FlashMode.off : RNCamera.Constants.FlashMode.torch} 
-          zoom={this.props.zoom}>
-          <View style={[styles.topButtonsContainer, this.props.topViewStyle]}>
-            {this.props.renderTopView()}
+          barCodeTypes={['qr']}
+          flashMode={
+            !flashMode
+              ? RNCamera.constants.FlashMode.on
+              : RNCamera.constants.TorchMode.off
+          }
+        >
+          <View style={[styles.topButtonsContainer, topViewStyle]}>
+            {renderTopView()}
           </View>
-          <QRScannerView
-            maskColor={this.props.maskColor}
-            cornerColor={this.props.cornerColor}
-            borderColor={this.props.borderColor}
-            rectHeight={this.props.rectHeight}
-            rectWidth={this.props.rectWidth}
-            borderWidth={this.props.borderWidth}
-            cornerBorderWidth={this.props.cornerBorderWidth}
-            cornerBorderLength={this.props.cornerBorderLength}
-            cornerOffsetSize={this.props.cornerOffsetSize}
-            isCornerOffset={this.props.isCornerOffset}
-            bottomHeight={this.props.bottomHeight}
-            scanBarAnimateTime={this.props.scanBarAnimateTime}
-            scanBarColor={this.props.scanBarColor}
-            scanBarHeight={this.props.scanBarHeight}
-            scanBarMargin={this.props.scanBarMargin}
-            hintText={this.props.hintText}
-            hintTextStyle={this.props.hintTextStyle}
-            scanBarImage={this.props.scanBarImage}
-            hintTextPosition={this.props.hintTextPosition}
-            isShowScanBar={this.props.isShowScanBar}
-            finderX={this.props.finderX}
-            finderY={this.props.finderY}
-            returnSize={this.barCodeSize}/>
-          <View style={[styles.bottomButtonsContainer, this.props.bottomViewStyle]}>
-            {this.props.renderBottomView()}
+          <QRScannerView {...ScanViewProps} returnSize={this.barCodeSize} />
+          <View style={[styles.bottomButtonsContainer, bottomViewStyle]}>
+            {renderBottomView()}
           </View>
         </RNCamera>
       </View>
-    );
+    )
   }
 
-  isShowCode = false;
+  isShowCode = false
 
-  barCodeSize = (size) => this.setState({barCodeSize:size})
+  barCodeSize = size => this.setState({ barCodeSize: size })
 
-  returnMax= (a,b) =>  a > b ? a : b
+  returnMax = (a, b) => (a > b ? a : b)
 
-  returnMin= (a,b) =>  a < b ? a : b
+  returnMin = (a, b) => (a < b ? a : b)
 
-  iosBarCode = (e) => {
+  iosBarCode = e => {
     let x = Number(e.bounds.origin.x)
     let y = Number(e.bounds.origin.y)
     let width = e.bounds.size.width
     let height = e.bounds.size.height
     let viewMinX = this.state.barCodeSize.x - this.props.finderX
     let viewMinY = this.state.barCodeSize.y - this.props.finderY
-    let viewMaxX = this.state.barCodeSize.x + this.state.barCodeSize.width - width - this.props.finderX
-    let viewMaxY = this.state.barCodeSize.y + this.state.barCodeSize.height - height - this.props.finderY
-    if ((x > viewMinX && y > viewMinY) && (x < viewMaxX && y < viewMaxY)) {
+    let viewMaxX =
+      this.state.barCodeSize.x +
+      this.state.barCodeSize.width -
+      width -
+      this.props.finderX
+    let viewMaxY =
+      this.state.barCodeSize.y +
+      this.state.barCodeSize.height -
+      height -
+      this.props.finderY
+    if (x > viewMinX && y > viewMinY && (x < viewMaxX && y < viewMaxY)) {
       if (this.props.isRepeatScan) {
-        Vibration.vibrate();
+        Vibration.vibrate()
         this.props.onRead(e)
       } else {
         if (!this.isShowCode) {
-          this.isShowCode = true;
-          Vibration.vibrate();
+          this.isShowCode = true
+          Vibration.vibrate()
           this.props.onRead(e)
         }
       }
     }
   }
 
-  androidBarCode = (e) => {
+  androidBarCode = e => {
     // if (!e.bounds[0] || !e.bounds[1] || !e.bounds[2] || !e.bounds[3]) return null;
     // const leftBottom = {x: e.bounds[0].x / pixelRatio, y: e.bounds[0].y / pixelRatio}
     // const leftTop= {x: e.bounds[1].x / pixelRatio, y: e.bounds[1].y / pixelRatio}
@@ -152,29 +154,29 @@ export default class QRScanner extends PureComponent {
     //   }
     // }
 
-     // 以下是不限制扫描区域
+    // 以下是不限制扫描区域
     if (this.props.isRepeatScan) {
-      Vibration.vibrate();
+      Vibration.vibrate()
       this.props.onRead(e)
     } else {
       if (!this.isShowCode) {
-        this.isShowCode = true;
-        Vibration.vibrate();
+        this.isShowCode = true
+        Vibration.vibrate()
         this.props.onRead(e)
       }
     }
   }
 
-  _handleBarCodeRead = (e) => {
+  _handleBarCodeRead = e => {
     switch (Platform.OS) {
       case 'ios':
-    this.iosBarCode(e);
-        break;
+        this.iosBarCode(e)
+        break
       case 'android':
-        this.androidBarCode(e);
-        break;
+        this.androidBarCode(e)
+        break
       default:
-        break;
+        break
     }
   }
 }
@@ -192,9 +194,9 @@ const styles = StyleSheet.create({
     height: 100,
     bottom: 0,
     left: 0,
-    right: 0
+    right: 0,
   }
-});
+})
 
 QRScanner.propTypes = {
   isRepeatScan: PropTypes.bool,
@@ -204,7 +206,8 @@ QRScanner.propTypes = {
   cornerColor: PropTypes.string,
   borderWidth: PropTypes.number,
   cornerBorderWidth: PropTypes.number,
-  cornerBorderLength: PropTypes.number,
+  cornerBorderHotizontalLength: PropTypes.number,
+  cornerBorderVerticalLength: PropTypes.number,
   rectHeight: PropTypes.number,
   rectWidth: PropTypes.number,
   isCornerOffset: PropTypes.bool, //边角是否偏移
@@ -226,6 +229,5 @@ QRScanner.propTypes = {
   flashMode: PropTypes.bool,
   finderX: PropTypes.number,
   finderY: PropTypes.number,
-  zoom: PropTypes.number,
   translucent: PropTypes.bool
 }
